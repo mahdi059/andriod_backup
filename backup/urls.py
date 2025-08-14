0f80399c24e8454036c58fda8b2e8c73bd4e6b98
mahdi <mahdi.ramezani.059@gmail.com>
2025-08-14 18:43:13 +0330

    add path for parse madia, sms, apk and ducuments also add path for organize_media

--- backup/urls.py
+++ backup/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('upload/', views.BackupUploadView.as_view(), name='upload-backup'),
+    path('<int:pk>/organize_media/', views.OrganizeMediaView.as_view(), name='organize-media'),
+    path('<int:pk>/parse-photos/', views.ParsePhotosView.as_view(), name='parse_photo'),
+    path('<int:pk>/parse-videos/', views.ParseVideosView.as_view(), name='parse_videos'),
+    path('<int:pk>/parse-audios/', views.ParseAudiosView.as_view(), name='parse_audios'),
+    path('<int:pk>/parse-sms/', views.ParseSMSBackupView.as_view(), name='parse_sms'),
+    path('<int:pk>/parse-apk/', views.ParseApksView.as_view(), name='parse_apk'),
+    path('<int:pk>/parse-documents/', views.ParseDocumentsView.as_view(),name='parse_documents' ),
 ]
