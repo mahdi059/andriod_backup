54844fa85f8f607d4284309c320c7c1fbf12d6a1
mahdi <mahdi.ramezani.059@gmail.com>
2025-08-16 05:23:25 +0330

    add ModelSerializer for NediaFile model

--- backup/serializers.py
+++ backup/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Backup
+from .models import Backup, MediaFile
 
 
 class BackupUploadSerializer(serializers.ModelSerializer):
@@ -10,3 +10,19 @@ class BackupUploadSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = self.context['request'].user
         return Backup.objects.create(user=user, **validated_data)
+
+
+
+class MediaFileSerializer(serializers.ModelSerializer):
+    file_url = serializers.SerializerMethodField()
+
+    class Meta:
+        model = MediaFile
+        fields = ['id', 'file_name', 'file_url', 'mime_type', 'size_bytes', 'added_at']
+
+    def get_file_url(self, obj):
+        request = self.context.get('request')
+        if obj.file:
+
+            return request.build_absolute_uri(obj.file.url)
+        return None
