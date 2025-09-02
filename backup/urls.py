8815ed1b608942722686a43525622c22b0b1965a
mahdi <mahdi.ramezani.059@gmail.com>
2025-09-02 12:14:39 +0330

    add URL route for ParseContactsAPIView and ParseCallLogsAPIView via : - <int:pk>/parse-contact/ - <int:pk>/parse-calllog/

--- backup/urls.py
+++ backup/urls.py
@@ -11,10 +11,11 @@ urlpatterns = [
     path('<int:pk>/parse-sms/', views.ParseSMSBackupView.as_view(), name='parse_sms'),
     path('<int:pk>/parse-apk/', views.ParseApksView.as_view(), name='parse_apk'),
     path('<int:pk>/parse-documents/', views.ParseDocumentsView.as_view(),name='parse_documents' ),
-    path('<int:pk>/parse-database/', views.ParseDatabaseAPIView.as_view(), name='parse_databases'),
     path('<int:pk>/parse-json/', views.ParseJSONBackupAPIView.as_view(), name='parse_json'),
     path('<int:pk>/media/',  views.MediaListAPIView.as_view(), name='media-list'),
     path('<int:pk>/sms-list/', views.MessageListAPIView.as_view(), name='sms-list'),
+    path('<int:pk>/parse-contact/', views.ParseContactsAPIView.as_view(), name='parse-contact'),
+    path('<int:pk>/parse-calllog/', views.ParseCallLogsAPIView.as_view(), name='parse-calllog'),
     path('<int:pk>/contact-list/', views.ContactListAPIView.as_view(), name='contact-list'),
     path('<int:pk>/calllog-list/', views.CallLogListAPIView.as_view(), name='calllog-list'),
 
