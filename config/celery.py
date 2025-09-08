5d7f307a89b460762cf435e548b2acd7c19ed3e0
mahdi059 <mahdi.ramezani.059@gmail.com>
2025-09-08 20:25:08 +0330

    add celery.py to setup celery in project

---
+++ config/celery.py
@@ -0,0 +1,13 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+
+app = Celery("config")
+
+
+app.conf.broker_url = "redis://redis:6379/0"
+app.conf.result_backend = "redis://redis:6379/0"
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
